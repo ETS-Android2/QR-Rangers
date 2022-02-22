a86585e5b32c66233baf418bb19cdaf7e81274b6
Shashank Bhat <shawshankbhat@gmail.com>
2022-02-22 14:52:02 -0700

    Added more methods and made type generic better

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/DbCollection.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/DbCollection.java
@@ -1,11 +1,10 @@
 package com.example.qr_rangers;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.NoSuchElementException;
 
@@ -14,7 +13,7 @@ import java.util.NoSuchElementException;
  * @param <T>
  *     The type of document stored in the collection
  */
-public class DbCollection<T> {
+public class DbCollection<T extends DbDocument> {
     private CollectionReference collection;
 
     /**
@@ -35,26 +34,66 @@ public class DbCollection<T> {
      *  Returns the document if found or null
      */
     public T getById(String id) {
-        return (T) collection.document(id);
+        DocumentSnapshot doc = collection.document(id).get().getResult();
+        if (doc != null) {
+            return (T) doc.toObject(DbDocument.class);
+        }
+        return null;
     }
 
     /**
      * Updates a document within the Firestore
-     * @param id
-     *  The generated id of the document within Firestore
      * @param data
      *  The new data for the document
      * @return
      *  Returns the document within Firestore after the operation
      */
-    public T update(String id, T data) {
-        DocumentReference document = collection.document(id);
-        if (document == null) {
-            throw new NoSuchElementException(String.format("Document with id %s does not exist in collection %s", id, collection.getPath()));
+    public T update(T data) {
+        if (data.id == null || data.id.isEmpty()) {
+            throw new IllegalArgumentException("Id not provided");
         }
-        Task t = document.set(data);
+        DocumentReference document = collection.document(data.id);
+        if (document == null) {
+            throw new NoSuchElementException(String.format("Document with id %s does not exist in collection %s", data.id, collection.getPath()));
+        }
+        Task<Void> t = document.set(data);
         t.getResult();
-        return (T) collection.document(id);
+        DocumentSnapshot documentSnapshot = collection.document(data.id).get().getResult();
+        if (documentSnapshot != null) {
+            return (T) documentSnapshot.toObject(DbDocument.class);
+        }
+        return null;
+    }
+
+    /**
+     * Adds a new document to the collection
+     * @param data
+     *  The document to add
+     * @return
+     *  Returns the document with generated id within Firestore
+     */
+    public T add(T data) {
+        data.id = null;
+        DocumentSnapshot doc = collection.add(data).getResult().get().getResult();
+        if (doc != null) {
+            return (T) doc.toObject(DbDocument.class);
+        }
+        return null;
+    }
+
+    /**
+     * Deletes a specified document within the collection if it exists
+     * @param id
+     *  The generated id of the document to delete
+     * @return
+     *  Returns the task for deleting the document
+     */
+    public Task<Void> delete(String id) {
+        T data = getById(id);
+        if (data == null) {
+            throw new NoSuchElementException(String.format("No such document with id %s", id));
+        }
+        return collection.document(id).delete();
     }
 }
 
--- QR-Rangers/app/src/main/java/com/example/qr_rangers/DbDocument.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/DbDocument.java
@@ -1,5 +1,5 @@
 package com.example.qr_rangers;
 
-public class DbDocument {
+public abstract class DbDocument {
     public String id;
 }
