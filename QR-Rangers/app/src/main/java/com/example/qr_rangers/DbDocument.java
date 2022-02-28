6324b12ecb0c2ee26ea3f9965900535f8e98f6c6
Shashank Bhat <shawshankbhat@gmail.com>
2022-02-27 17:30:34 -0700

    Fixes DbCollection methods to not include ids in the database

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/DbDocument.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/DbDocument.java
@@ -1,5 +1,20 @@
 package com.example.qr_rangers;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class DbDocument {
-    public String id;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public abstract DbDocument fromMap(Map<String, Object> map);
+    public abstract Map<String, Object> toMap();
 }
