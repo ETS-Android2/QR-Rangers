bd3cdbcb242b85ec061857a9c6d0653550eed4f7
Ryan Haskins <rhaskins@ualberta.ca>
2022-03-03 20:58:53 -0700

    Created IntroActivity and MainActivity now a fork activity

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/MainActivity.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/MainActivity.java
@@ -6,13 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +24,16 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 Toast.makeText(this, "Error: User not found! Please create a new account.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, IntroActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
             }
         }
-
-        create = findViewById(R.id.create);
-        create.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
-                startActivity(intent);
-
-            }
-        });
+        else {
+            Intent intent = new Intent(MainActivity.this, IntroActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 
     /**
