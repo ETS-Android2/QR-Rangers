b41bb24773d755141f501fd83d4438a54a6c7315
Adamya <adamya.nagpal2000@gmail.com>
2022-03-04 21:01:38 +0530

    shows score takes photo

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/ScanResultActivity.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/ScanResultActivity.java
@@ -3,25 +3,51 @@ package com.example.qr_rangers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ScanResultActivity extends AppCompatActivity {
 
-    private TextView Score;
+    private TextView scoreTextView;
+    private ImageButton cameraButton;
+    Bitmap photo = null;
+    private static final int pic_id = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Score = findViewById(R.id.textViewScore);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_result);
         Intent intent = getIntent();
         String content = intent.getStringExtra("content");
         QRCode qr = new QRCode(content,null,null);
         int score = qr.getScore();
-        Score.setText(String.valueOf(score));
+        scoreTextView = findViewById(R.id.textViewScore);
+        scoreTextView.setText(String.valueOf(score));
+        cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent camera_intent
+                        = new Intent(MediaStore
+                        .ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
 
-
-
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == pic_id) {
+             photo = (Bitmap) data.getExtras()
+                    .get("data");
+        }
     }
 }
