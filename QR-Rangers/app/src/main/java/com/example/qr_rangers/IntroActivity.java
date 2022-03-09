73ecd9ce3ed826a8084b71aa0ebd8925eb05979f
Ryan Haskins <rhaskins@ualberta.ca>
2022-03-08 23:55:43 -0700

    QR login function (kinda) works

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/IntroActivity.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/IntroActivity.java
@@ -1,11 +1,18 @@
 package com.example.qr_rangers;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -22,5 +29,64 @@ public class IntroActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button loginQR = findViewById(R.id.buttonLogin);
+        loginQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(IntroActivity.this);
+                intentIntegrator.setPrompt("Scan a valid user QR Code");
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // if the intentResult is not null we'll set
+                // the content and format of scan message
+                String id = intentResult.getContents();
+                try {
+                    if (Database.Users.getById(id, new User("", "", "")) != null) {
+                        saveID(id);
+                        Toast.makeText(this,
+                                "Hello again " + Database.Users.getById(id, new User("", "", "")).getUsername() + "!",
+                                Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(IntroActivity.this, HomeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    /**
+     * Saves the ID of the new user to local storage for future reference
+     * @param id
+     *     The string ID of the newly made user
+     */
+    private void saveID(String id) {
+        SharedPreferences sharedPreferences =  PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("ID", id);
+        editor.apply();
     }
 }
