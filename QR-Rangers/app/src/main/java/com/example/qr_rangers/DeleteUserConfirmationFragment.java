9b552314f81f11f9c4e0734c5039f077db2edfec
Alikay <asalm@ualberta.ca>
2022-03-13 06:00:33 -0600

    added confirmation fragment for user deletion
    local deletion works, but does not affect database

---
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/DeleteUserConfirmationFragment.java
@@ -0,0 +1,70 @@
+package com.example.qr_rangers;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+/**
+ * Fragment to confirm user deletion
+ *
+ * @author Alexander Salm
+ * @version 1.0
+ */
+public class DeleteUserConfirmationFragment extends DialogFragment {
+    ArrayAdapter<User> users;
+    int index;
+    public DeleteUserConfirmationFragment(ArrayAdapter<User> users, int i){
+        super();
+        this.users = users;
+        this.index = i;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_delete_user, null);
+
+        TextView text = view.findViewById(R.id.delete_user_confirmation);
+        text.setText("Are you sure you want to delete user \"" + users.getItem(index).getUsername() + "\"? (This cannot be undone)");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Delete User")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        users.remove(users.getItem(index));
+                        DbCollection users_collection = new DbCollection("users");
+                        //users_collection.delete(users.getItem(index).getId());
+
+                    }
+                }).create();
+
+    }
+}
--- QR-Rangers/app/src/main/java/com/example/qr_rangers/ViewAdminUsersFragment.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/ViewAdminUsersFragment.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
@@ -40,8 +41,7 @@ public class ViewAdminUsersFragment extends Fragment {
 
         users_list = view.findViewById(R.id.admin_users_list);
 
-        users = new ArrayList<User>();
-
+        users = new DbCollection("users").searchSuggestions("");
 
         users_adapter = new CustomList(getActivity(), users);
 
@@ -50,7 +50,19 @@ public class ViewAdminUsersFragment extends Fragment {
         users_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.e("NOTE", "Clicked on " + Integer.toString(i));
+                Intent profileIntent = new Intent(getActivity(), ProfileActivity.class);
+                profileIntent.putExtra("user", users.get(i));
+                startActivity(profileIntent);
+            }
+        });
+
+        users_list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l){
+                Log.i("NOTE", "Long press on " + Integer.toString(i));
+                DialogFragment delete_user_fragment = new DeleteUserConfirmationFragment(users_adapter, i);
+                delete_user_fragment.show(getActivity().getSupportFragmentManager(), "Delete_User");
+                return true;
             }
         });
         return view;
