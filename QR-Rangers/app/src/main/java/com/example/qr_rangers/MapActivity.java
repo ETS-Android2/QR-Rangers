7dfe0281ba452a69562bd82877c7f5fe0ae85c80
Alikay <asalm@ualberta.ca>
2022-03-21 03:08:15 -0600

    very rudimentary functionality

---
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/MapActivity.java
@@ -0,0 +1,141 @@
+//adapted from https://stackoverflow.com/questions/52431299/how-can-i-use-osm-in-android-studio-to-show-a-map
+
+package com.example.qr_rangers;
+
+
+import static android.os.Build.VERSION_CODES.M;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.mylocation.DirectedLocationOverlay;
+
+/**
+ * This is an activity that shows the user a map of all nearby scanned QR codes
+ * @author Alexander Salm
+ * @version 1.0
+ */
+public class MapActivity extends AppCompatActivity implements LocationListener {
+
+    MapView map = null;
+    private MapView mapView;
+    private MapController mapController;
+    private LocationManager locationManager;
+    private CompassOverlay compassOverlay;
+    private DirectedLocationOverlay locationOverlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+
+        if (Build.VERSION.SDK_INT >= M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                String[] perms = {Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(perms, 1);
+            }
+        }
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+
+        mapController = (MapController) mapView.getController();
+        mapController.setZoom(3);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,1,this);
+
+        GeoPoint center = new GeoPoint(0,0);
+        mapController.animateTo(center);
+        addMarker(center);
+
+        mapView.setMapListener(new MapListener() {
+            @Override
+            public boolean onScroll(ScrollEvent event) {
+                Log.i("NOTE", "onScroll()");
+                return false;
+            }
+
+            @Override
+            public boolean onZoom(ZoomEvent event) {
+                Log.i("NOTE", "onZoom()");
+                return false;
+            }
+        });
+
+    }
+
+    public void addMarker (GeoPoint center){
+        Marker marker = new Marker(mapView);
+        marker.setPosition(center);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        marker.setIcon(getResources().getDrawable(R.drawable.ic_menu_mylocation));
+        mapView.getOverlays().clear();
+        mapView.getOverlays().add(marker);
+        mapView.invalidate();
+        marker.setTitle("You are here");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.recreate();
+
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull android.location.Location location) {
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+
+        GeoPoint center = new GeoPoint(lat, lon);
+
+        mapController.animateTo(center);
+        addMarker(center);
+
+        Log.i("NOTE", "New location: {Lat: " + Double.toString(lat) + ", Long: " + Double.toString(lon) + "}");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if (locationManager != null){
+            locationManager.removeUpdates(this);
+        }
+    }
+}
