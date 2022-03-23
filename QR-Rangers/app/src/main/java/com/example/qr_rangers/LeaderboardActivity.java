02a4abb35dd95a09e99bd9d93b989460a25d1eec
Jawdat <toume@ualberta.ca>
2022-03-23 14:13:52 -0600

    Activity stuff

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/LeaderboardActivity.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/LeaderboardActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class LeaderboardActivity extends AppCompatActivity {
-    Search search = new Search();
+    DbCollection<User> db = new DbCollection<>("users");
     ArrayList<User> leaders = new ArrayList<User>();
 
     ListView board;
@@ -16,7 +21,46 @@ public class LeaderboardActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
 
+        TabLayout tabs = findViewById(R.id.admin_tab_switcher);
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(TabLayout.Tab tab){
+                Fragment fragment = null;
+                switch(tab.getPosition()){
+                    case 0:
+                        fragment = new ViewHighScoreFragment();
+                        break;
+                    case 1:
+                        fragment = new ViewNumCodesFragment();
+                        break;
+                    case 2:
+                        fragment = new ViewUniqueQrFragment();
+                }
 
+                switchFragment(fragment);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab){
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab){
+
+            }
+        });
+
+        switchFragment(new ViewAdminUsersFragment());
+    }
+
+    public void switchFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.admin_frame_layout, fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
     }
 }
---
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/ViewNumCodesFragment.java
@@ -0,0 +1,63 @@
+package com.example.qr_rangers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+public class ViewNumCodesFragment extends Fragment {
+    private ListView usersList;
+    private ArrayList<User> users = new ArrayList<>();
+    private ArrayAdapter<User> usersAdapter;
+    public ViewNumCodesFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_admin_users, null);
+
+        usersList = view.findViewById(R.id.admin_users_list);
+
+        users = Database.Users.getAll();
+
+        usersAdapter = new CustomList(getActivity(), users);
+
+        usersList.setAdapter(usersAdapter);
+
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent profileIntent = new Intent(getActivity(), ProfileActivity.class);
+                profileIntent.putExtra("user", users.get(i));
+                startActivity(profileIntent);
+            }
+        });
+
+        usersList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("NOTE", "Long press on " + Integer.toString(i));
+                DialogFragment deleteUserFragment = new DeleteUserConfirmationFragment(usersAdapter, i);
+                deleteUserFragment.show(getActivity().getSupportFragmentManager(), "Delete_User");
+                return true;
+            }
+        });
+        return view;
+    }
+}
