23b52ebb7518ca0b2fd80dc54e8f982de0219c7b
Alikay <asalm@ualberta.ca>
2022-04-01 16:51:40 -0600

    fixed click on qr on map issue

--- QR-Rangers/app/src/main/java/com/example/qr_rangers/QRInfoActivity.java
+++ QR-Rangers/app/src/main/java/com/example/qr_rangers/QRInfoActivity.java
@@ -64,7 +64,7 @@ public class QRInfoActivity extends AppCompatActivity {
         commentText.setVisibility(View.GONE);
 
         commentList = findViewById(R.id.qr_info_comment_list);
-        commentAdapter = new ArrayAdapter<>(this, R.layout.activity_qr_info, comments);
+        commentAdapter = new ArrayAdapter<String>(this, R.layout.comment, comments);
         commentList.setAdapter(commentAdapter);
 
         image = findViewById(R.id.qr_info_image);
